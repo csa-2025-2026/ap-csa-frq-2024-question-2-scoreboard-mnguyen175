024aab3b26bfb45aff4501b9aa7c0304dd5b06dc
mnguyen175 <maryannguyen2017@gmail.com>
2025-12-15 19:03:22 +0000

    code

--- Scoreboard.java
+++ Scoreboard.java
@@ -8,3 +8,49 @@
 */
 
 // Write the class below
+public class Scoreboard {
+
+    private String team1; 
+    private String team2; 
+    private int team1Score; 
+    private int team2Score; 
+    private String activeTeam; 
+
+    public Scoreboard(String team1, String team2)
+    {
+        this.team1 = team1; 
+        this.team2 = team2; 
+        this.activeTeam = team1; 
+    }
+
+    public String getScore()
+    {
+        return team1Score + "-" + team2Score + "-" + activeTeam;
+    }
+
+    public void recordPlay(int point)
+    {
+        if (activeTeam.equals(team1))
+        {
+            if (point == 0)
+            {
+                activeTeam = team2;
+            }
+            else 
+            {
+                team1Score += point;
+            } 
+        }
+        else if (activeTeam.equals(team2))
+        {
+            if (point == 0)
+            {
+                activeTeam = team1; 
+            }
+            else
+            {
+                team2Score += point; 
+            }
+        }
+    }
+}
